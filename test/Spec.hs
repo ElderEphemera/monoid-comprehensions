249c7df7070b2798bb38a760eb93464387776282
Joshua Price <2855417+WhistlePayer@users.noreply.github.com>
2018-10-17 00:19:12 -0400

    Use HUnit in testsuite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,14 @@
 {-# OPTIONS_GHC -fplugin=MonoidComprehensions #-}
 {-# LANGUAGE MonadComprehensions #-}
 
-import Control.Exception
 import Data.Foldable
 import Data.Monoid
+import Test.HUnit
 
 -- TODO: Write comprehensive test suite
-main :: IO ()
-main = assert (sumListComp == sumMonoidComp) $ putStrLn "\nAll Good!"
-
-sumListComp, sumMonoidComp :: Int
-sumListComp = sum [ x * y | x <- [1..3], y <- [1..3] ]
-sumMonoidComp = getSum ([ Sum (x * y) | x <- [1..3], y <- [1..3] ])
+main :: IO Counts
+main = runTestTT $ test
+  [ "Sum"
+    ~:  sum [x*y | x <- [1..3], y <- [1..3::Int]]
+    ~=? getSum ([Sum (x*y) | x <- [1..3], y <- [1..3::Int]])
+  ]
